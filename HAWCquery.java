5bfafdf1714b945d409f8480714544d27725634d
Eduardo Murrieta <eml@ciencias.unam.mx>
2023-12-02 20:05:51 -0600

    Version final para entrega

--- HAWCquery.java
+++ HAWCquery.java
@@ -1,3 +1,56 @@
+/*
+ * Clase: HAWCquery
+ *
+ * Clase principal de la aplicación HAWCquery.
+ *
+ * Programador: Eduardo Murrieta
+ * Revisores:   Donovan Montejano, Alejandro Salcido.
+ * ------------------------------------------------------------------------
+ *
+ * El programa recibe un archivo CSV de entrada y lo filtra
+ * usando una consulta al estilo de un query SQL. 
+ *
+ * Los argumentos y opciones de entrada se obtienen ejecutando el
+ * programa sin parámetros.
+ *   
+ *   $ java HAWCquery 
+ *   Requiere los argumentos: [-n Hilos] [-o CSV_salida] [-d] CSV_entrada "Query"
+ *
+ *   [-n Hilos]	Opcional, indica el número de hilos a generar, por omisión
+ *   		el número de hilos es igual al total de cores disponibles*4
+ *   		-n 1 implica ejecución serial.
+ *   		-n 4 usar 4 hilos
+ *
+ *   [-o CSV_salida] Opcional, porporciona un nombre alternativo para el
+ *   		archivo de salida, por omisión el archivo de salida se 
+ *   		ubica en el directorio 'resultados' con el nombre:
+ *   		CSV_entrada_filtered(AAAAMMDD_HHMM).csv
+ *
+ *   [-d]	Activa los mensajes de depuración, es recomendable sólo con 
+ *   		CSVs de menos de 100 registros.
+ *
+ *   CSV_entrada Indica el nombre del archivo CSV de entrada. En el directorio
+ *   		data se encuentra un ejemplo con 100 registros
+ *   			data/reco_run002054_00226_sample100.csv
+ *   		Y una versión con 500 mil registros comprimida:
+ *   			data/reco_run002054_00226_sample.csv.zip
+ *   "Query"	Es una cadena entrecomillada con la siguiente sintaxis:
+ *   			"select(columnaX,columnaY,...)"
+ *   		Donde: 'columnaX','columnaY', etc. es el nombre de una 
+ *   		columna en el CSV_entrada que será colocada en el CSV de salida.
+ *   		
+ *   		Opcionalmente se puede colocar después de la cláusula 'select'
+ *   		una cláusula 'where' con la sintaxis:
+ *   		    "where(columanX OPLOG valorX, columnaN OPLOG valorN)"
+ *
+ *   		en donde: OPLOG puede ser algún operando '<','>','=','<=','>=';
+ *   		que se aplica entre el valor de 'columnaN' y 'valorN'.
+ *
+ *   		La sintaxis completa es:
+ *   		    "select(columnaX,columnaY,..) where(columanX OPLOG valorX)"
+ *
+ * 
+ */
 import hawc.CSV;
 import hawc.Manager;
 import hawc.Utils;
@@ -6,6 +59,7 @@ import hawc.Query;
 
 public class HAWCquery {
 
+	// Método principal del programa HAWCquery
 	public static void main(String[] args){
 		Manager manager;
 		CSV csv;
@@ -13,6 +67,7 @@ public class HAWCquery {
 		String query="";
 		String csvSalida="";
 
+		// Revisión de los argumentos del programa
 		int arguments=args.length;
 		for (int i=0; i<args.length; i++) {
 			if (args[i].equals("-n")) {
@@ -36,11 +91,12 @@ public class HAWCquery {
 		
 		if (!Query.sintaxisQuery(args[args.length-arguments+1]))
 		{
-			System.err.println("Error de sintaxis en el Query"+args[args.length-arguments+1]);
+			System.err.println("Error de sintaxis en el Query: "+args[args.length-arguments+1]);
 			System.exit(0);
 		}
 		query = args[args.length-arguments+1];
 
+		// Se crea el objeto CSV para manejar los procesos de I/O
 		csv = new CSV();
 		String csvEntrada=args[args.length-arguments];
 		if ( !csv.inputFile(csvEntrada) ) {
@@ -48,15 +104,19 @@ public class HAWCquery {
 			System.exit(0);
 		}
 
+		// Crea el directorio 'resultados'
 		if (!Utils.createDir("resultados")) {
 			System.err.println("Error al crear el directorio de resultados");
 			System.exit(0);
 		}
 
+		// Si no se proporciona un nombre para el CSV de salida, se genera
+		// un nombre con el nombre estándar.
 		if ( csvSalida=="" ) { 
 			csvSalida = Utils.getOutputFilenameFor(csvEntrada);
 		}
 
+		// Se crea el CSV de salida
 		if ( !csv.outputFile(csvSalida) ) {
 			System.err.println("Error en la apertura del archivo de salida");
 			System.exit(0);
@@ -80,8 +140,7 @@ public class HAWCquery {
 			System.exit(-1);
 		}
 
-		// Recupera los tiempos de procesamiento
-		// e imprime estadisticas
+		// Recupera los tiempos de procesamiento e imprime estadisticas
 		long[] timers;
 		long totalTime=0;
 		timers = manager.timers();
@@ -91,6 +150,9 @@ public class HAWCquery {
 		System.out.println ("*                               METRICAS                               *");
 		System.out.println ("========================================================================");
 		System.out.println ("Hilos: "+manager.getCPUs());
+		long stat[] = csv.estadisticas();
+		System.out.println ("Registros de entrada: "+String.format("%,d",stat[0]));
+		System.out.println ("Registros de salida:  "+String.format("%,d",stat[1]));
 		if (manager.getCPUs()>1) { 
 			System.out.println ("Segmentado de archivo en modo serial:\t"+
 					String.format("%.2f",(double)timers[0]/1000000000)+" s"); 
--- README.md
+++ README.md
@@ -1,2 +1,65 @@
-# PCIC_ProgAvz
-Repositorio del proyecto de Programación Avanzada del PCIC UNAM
+# HAWCquery
+Repositorio del proyecto de Programación Avanzada del PCIC UNAM Gen. 2024-1
+
+El presente repositorio contienen el desarrollo del proyecto final del curso
+de Programación Avanzada del Posgrado en Ciencia e Ingeniería de la Computación
+de la UNAM Gen. 2024-1.
+
+El programa principal (main) se encuentra en el archivo HAWCquery.java. 
+Existen dos directorios: data y hawc.
+El directorio hawc contiene el resto de las clases del programa y se encuentran
+agrupadas en el paquete hawc.
+El directorio data contiene un ejemplo con pocos registros (100) de un archivo
+csv de entrada y otro con 500 mil registros.
+
+100 registros:      data/reco_run002054_00226_sample100.csv
+500 mil registros:  data/reco_run002054_00226_sample.csv.zip
+
+El código se compila con:
+
+javac HAWCquery.java
+
+Ejemplos de ejecución:
+1) Sin argumentos
+$ java HAWCquery 
+Requiere los argumentos: [-n Hilos] [-o CSV_salida] [-d] CSV_entrada "Query"
+
+El programa muestra que para ejecutarse existen argumentos que son obligatorios
+entre corchetes [] y definitivos sin corchetes.
+
+[-n Hilos] : inidica el número de hilos a usar, si es 1 la ejecución es serial,
+             si es mayor a uno se ejecuta con el numero de hilos indicado. Por
+             omisión el número de hilos es igual al total de cores disponibles
+             multiplicado por 4.
+
+[-o CSV_salida] : Indica un nombre para el archivo de salida, en caso de no
+             indicarlo los resultados se guardan en el directorio resultados
+             (se crea si no existe) con el nombre:
+                                     CSV_entrada_filtered(AAAAMMDD_HHMM).csv
+[-d] :       Habilita el modo de depuración, imprime mensajes utilizados para
+             dar seguimiento al flujo del proceso.
+
+CSV_entrada : Nombre del archivo CSV de entrada.
+
+"Query" :    Entrecomillado, indica los campos que deben seleccionarse del
+             CSV como por ejemplo "select(rec.eventID/U/1,rec.logNPE/F/0.01)"
+             indica que se deben seleccionar las columnas "rec.eventID/U/1"
+             y "rec.logNPE/F/0.01".
+
+             En caso de que exista algún error en la sintaxis del Query o
+             de en el nombre de algún campo, se notifica de un error y el 
+             programa termina.
+
+Al terminar el programa se muestran las mediciones del tiempo invertido en
+cada etapa del procesamiento como se muestra a continuación.
+
+========================================================================
+*                               METRICAS                               *
+========================================================================
+Hilos: 96
+Segmentado de archivo en modo serial:   50.55 s
+Procesamiento del query en paralelo:    13.63 s
+Unificacion de salida en modo serial:   37.23 s
+Tiempo total:				101.41 s
+========================================================================
+
